856b3252722a16fdff0d91925389fce52ea368ad
luanpanno <luanpanno@gmail.com>
2020-02-02 15:17:43 -0300

    portfolio inicial finalizado

--- js/main.js
+++ js/main.js
@@ -9,17 +9,13 @@ const linkItemNavEl = document.querySelectorAll('.item-nav a');
 
 const firstPageEl = document.querySelectorAll('.first-page');
 
+typewritter();
+
 document.onscroll = () => {
     if(document.documentElement.scrollTop == 0){
-        navStyle('100px', 'white', 'transparent', false);
-        linkItemNavEl.forEach(a => {
-            navHover(a, 'white');
-        });
+        headerEl.setAttribute('class', 'noscroll');
     } else{
-        navStyle('80px', '#00E69D', 'white', true);
-        linkItemNavEl.forEach(a => {
-            navHover(a, '#00E69D');
-        });
+        headerEl.setAttribute('class', 'onscroll');
     }
 }
 
@@ -46,33 +42,3 @@ function typewritter(){
     }
     type();
 }
-
-function navStyle(height, textColor, bgColor, scrolled){
-    headerEl.style.height = height;
-    headerEl.style.backgroundColor = bgColor;
-    headerEl.style.transition = '400ms';
-    logoHeader.style.color = textColor;
-    linkItemNavEl.forEach(item => {
-        item.style.color = textColor;
-        item.style.border = 'none';
-    })
-    if(scrolled == true){
-        headerEl.style.boxShadow = '0px 1px 10px 0px rgba(0, 0, 0, .4)';
-        headerEl.style.zIndex = '2';
-        document.querySelector('.container').style.overflowY = 'initial'
-    } else{
-        headerEl.style.boxShadow = 'none';
-    }
-}
-
-function navHover(element, color){
-    element.onmouseover = () => {
-        element.style.borderBottom = `1px solid ${color}`;
-        element.style.transition = 'border 200ms';
-    }
-    element.onmouseout = () => {
-        element.style.borderBottom = 'none';
-    }
-}
-
-typewritter();
